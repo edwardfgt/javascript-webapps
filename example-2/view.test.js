1ecef3a5c39778218af8d9f82ff6a22bdcd1989b
edwardfgthomas@gmail.com <edwardfgthomas@gmail.com>
2023-02-27 16:01:58 +0000

    create new test for addParagraph

--- example-2/view.test.js
+++ example-2/view.test.js
@@ -6,11 +6,23 @@ const fs = require('fs');
 const View = require('./view');
 
 describe('Page view', () => {
-  it('displays 2 paragraphs', () => {
+
+  beforeEach(() => {
     document.body.innerHTML = fs.readFileSync('./index.html');
+  });
+
+  it('displays 2 paragraphs', () => {
 
     const view = new View();
 
     expect(document.querySelectorAll('p').length).toBe(2);
+  });
+
+
+  it('displays 3 paragraphs', () => {
+
+    const view = new View();
+    view.addParagraph();
+    expect(document.querySelectorAll('p').length).toBe(3);
   });
 });
